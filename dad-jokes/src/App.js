037379b7bc0b338d6700efab9f30b2250aad930a
TwistZA <avi3110@gmail.com>
2020-04-05 20:55:21 +0200

    first commit

---
+++ dad-jokes/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import DadJokes from "./DadJokes.js";
+
+function App() {
+  return (
+    <div className="App">
+      <DadJokes />
+    </div>
+  );
+}
+
+export default App;
